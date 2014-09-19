4d0c1fc3c8fe4e28b4927b784f60b53004168351
ryan_xantoo <ryan_xantoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 20:36:31 +0000

    Minor map displaying issues fixed!
    git-svn-id: https://plugins.svn.wordpress.org/map-contact/trunk@993559 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- map-contact.php
+++ map-contact.php
@@ -5,7 +5,7 @@
 	Author: Ryan Smith
     Plugin URI: http://wordpress.org/plugins/map-contact/
     Author URI: http://xantoo.com/
-	Version: 3.0.2
+	Version: 3.0.3
  */
 
 include(plugin_dir_path( __FILE__ )."includes/maps.php");
